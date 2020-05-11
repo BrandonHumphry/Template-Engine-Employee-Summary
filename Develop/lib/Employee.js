daaf763deb0018fbe0ea150a682d34b0e38a4836
Brandon Humphry <brandonhumphry@gmail.com>
2020-05-10 22:53:39 -0700

    Adding homework 10, tests are not yet running. I think I am missing a function

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,23 @@
+// TODO: Write code to define and export the Employee class
+class Employee {  
+    constructor(Employee){
+        this.name = name;
+        this.email = email;
+        this.getName = getName();
+        this.getId = getId();
+        this.getEmail = getEmail();
+        this.getRole = getRole();
+    }
+}  
+
+//   name
+//   * id
+//   * email
+//   * getName()
+//   * getId()
+//   * getEmail()
+//   * getRole() // Returns 'Employee'
+
+  module.Exports = class Employee {
+ 
+  }
